3c9b07234027a0070efb8ba85764410e340e5e4a
Waffflezz <dima.skorobogatov.99@mail.ru>
2024-06-07 22:13:08 +0700

    Add tailwind.css and layout

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
